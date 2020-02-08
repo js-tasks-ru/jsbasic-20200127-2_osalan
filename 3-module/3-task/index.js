cad6d97c34535d8b3cd40e9195de288c7e22821e
Osokin Alexander Andreevich <osalan@yandex.ru>
2020-02-08 14:03:42 +0300

    Module 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,4 +3,11 @@
  * @returns {string}
  */
 function camelize(str) {
+  let words = str.split('-');
+  for (let i = 0; i < words.length; i++) {
+    if (i != 0) {
+      words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+    }
+  }
+  return words.join('');
 }
